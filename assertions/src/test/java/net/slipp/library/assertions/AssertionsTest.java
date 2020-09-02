b3a526dec9d033a61a64de43aeafcfdf1ecfaf27
kwangpil.hong <kwangpil.hong@yanolja.com>
2020-09-02 23:22:46 +0900

    add module - assertions

---
+++ assertions/src/test/java/net/slipp/library/assertions/AssertionsTest.java
@@ -0,0 +1,65 @@
+package net.slipp.library.assertions;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void assertArgumentEquals() {
+        Assertions.assertArgumentEquals("name", "name1", "not equal");
+    }
+
+    @Test
+    public void assertArgumentFalse() {
+    }
+
+    @Test
+    public void assertArgumentLength() {
+    }
+
+    @Test
+    public void testAssertArgumentLength() {
+    }
+
+    @Test
+    public void assertArgumentNotEmpty() {
+    }
+
+    @Test
+    public void assertArgumentNotEquals() {
+    }
+
+    @Test
+    public void assertArgumentNotNull() {
+    }
+
+    @Test
+    public void assertArgumentRange() {
+    }
+
+    @Test
+    public void testAssertArgumentRange() {
+    }
+
+    @Test
+    public void testAssertArgumentRange1() {
+    }
+
+    @Test
+    public void testAssertArgumentRange2() {
+    }
+
+    @Test
+    public void assertArgumentTrue() {
+    }
+
+    @Test
+    public void assertStateFalse() {
+    }
+
+    @Test
+    public void assertStateTrue() {
+    }
+}
